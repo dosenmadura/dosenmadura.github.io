1709574afc6dd6ea188977a93b62f33966f83b66
dosenmadura <dosenmadura@gmail.com>
2026-07-15 03:07:00 +0700

    membuat fungsi render jurnal

--- jurnal/assets/js/script.js
+++ jurnal/assets/js/script.js
@@ -1,38 +1,56 @@
+let semuaJurnal = [];
+
 fetch("assets/data/jurnal.json")
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
 
-    const container = document.getElementById("jurnal-list");
+        semuaJurnal = data;
 
-    data.forEach(jurnal => {
-
-      container.innerHTML += `
-        <div class="card">
-
-          <img src="${jurnal.cover}" alt="${jurnal.nama}">
-
-          <div class="card-body">
-
-            <h3>${jurnal.nama}</h3>
-
-            <p class="kampus">🏫 ${jurnal.kampus}</p>
-
-            <span class="badge">
-              🏅 SINTA ${jurnal.sinta}
-            </span>
-
-            <a href="${jurnal.url}" target="_blank" class="btn">
-              🌐 Kunjungi Jurnal
-            </a>
-
-          </div>
-
-        </div>
-      `;
+        renderJurnal(semuaJurnal);
 
     });
 
-  })
-  .catch(error => {
-    console.error("Gagal membaca jurnal.json", error);
-  });
+function renderJurnal(data){
+
+    const container = document.getElementById("jurnal-list");
+
+    container.innerHTML = "";
+
+    data.forEach(jurnal=>{
+
+        container.innerHTML += `
+
+        <div class="card">
+
+            <img src="${jurnal.cover}" alt="${jurnal.nama}">
+
+            <div class="card-body">
+
+                <h3>${jurnal.nama}</h3>
+
+                <p class="kampus">
+                    🏫 ${jurnal.kampus}
+                </p>
+
+                <span class="badge">
+                    🏅 SINTA ${jurnal.sinta}
+                </span>
+
+                <a
+                    href="${jurnal.url}"
+                    target="_blank"
+                    class="btn">
+
+                    🌐 Kunjungi Jurnal
+
+                </a>
+
+            </div>
+
+        </div>
+
+        `;
+
+    });
+
+}
