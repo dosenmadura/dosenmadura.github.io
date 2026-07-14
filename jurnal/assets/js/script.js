eec9f0c553188fe3b413bbcc4a66df110be3b232
dosenmadura <dosenmadura@gmail.com>
2026-07-15 02:47:01 +0700

    menampilkan data jurnal dari json

--- jurnal/assets/js/script.js
+++ jurnal/assets/js/script.js
@@ -1,1 +1,41 @@
-console.log("FPDM Ready");
+fetch("assets/data/jurnal.json")
+.then(response => response.json())
+.then(data => {
+
+const container=document.getElementById("jurnal-list");
+
+data.forEach(jurnal=>{
+
+container.innerHTML +=`
+
+<div class="card">
+
+<img src="${jurnal.cover}">
+
+<div class="card-body">
+
+<h3>${jurnal.nama}</h3>
+
+<p class="kampus">
+🏫 ${jurnal.kampus}
+</p>
+
+<span class="badge">
+🏅 SINTA ${jurnal.sinta}
+</span>
+
+<a href="${jurnal.url}" target="_blank" class="btn">
+
+🌐 Kunjungi Jurnal
+
+</a>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+});
